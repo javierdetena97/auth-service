eac374e04b1729a269994dffad86809fa3ec76c8
Javier de Tena <javidetena97@gmail.com>
2024-08-13 23:10:25 +0200

    Create User entity

---
+++ src/main/java/com/example/authservice/commons/entities/User.java
@@ -0,0 +1,30 @@
+package com.example.authservice.commons.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+}
